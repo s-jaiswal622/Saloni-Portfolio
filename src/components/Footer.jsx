f484007c167884ae9dde4f21d640e7fd986f6f0a
Saloni Jaiswal <salonijaiswal622@gmail.com>
2025-06-20 00:14:34 +0530

    fix: changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
 const Footer = () => {
-
   return (
-    <footer className="text-center text-gray-400 text-sm py-6 border-t border-white/10">
-    © {new Date().getFullYear()} Saloni Jaiswal — Built with React, Tailwind CSS & Firebase.
+    <footer className="text-center text-gray-400 text-sm py-6 border-t border-white/10 mt-12">
+    © {new Date().getFullYear()} Saloni Jaiswal — Designed & Developed with ❤️
   </footer>
-  
   
   );
 };
